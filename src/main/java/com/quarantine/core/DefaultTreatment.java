a5f14b08cc8b4d12784839f2db798d907cb38b77
NAGARRO\indiverkumar <indiver.kumar@nagarro.com>
2018-08-29 00:37:50 +0530

    testRebase3

--- src/main/java/com/quarantine/core/DefaultTreatment.java
+++ src/main/java/com/quarantine/core/DefaultTreatment.java
@@ -14,6 +14,8 @@ public class DefaultTreatment extends AbstractTreatment {
 		if(!quarantine.checkIfhasTreatment(Medicine.INSULIN)){
 			System.out.println("testRebase");
 			System.out.println("testRebase2");
+			System.out.println("testRebase3");
+			System.out.println("testRebase4");
 			quarantine.getPatientGroups().get(HealthStatus.DEAD).combine(quarantine.getPatientGroups().get(HealthStatus.DIABATIC));
 		}
 		
